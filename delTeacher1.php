0393c7a3dad4722456400e0af848c60acac16289
Shivangi Kapadia <31736778+KShivangi@users.noreply.github.com>
2019-12-16 15:39:25 +0530

    Update delTeacher1.php

--- delTeacher1.php
+++ delTeacher1.php
@@ -7,26 +7,13 @@
 	
 	$query = mysqli_query($con, "Select * from users where id='$id'");
 	$exists = mysqli_num_rows($query);
+	$null = NULL;
 	
 	if($exists>0)
 	{
 		mysqli_query($con, "DELETE from users where id='$id'");
+		mysqli_query($con, "UPDATE subjects SET t_id = '$null' WHERE t_id = '$id'");
 
-		$sub = mysqli_query($con, "select * from subjects where t_id = '$id'");
-		$exists2 = mysqli_num_rows($sub);
-		$sub_code = "";
-		if($exists2>0)
-		{
-			while($row = mysqli_fetch_assoc($sub))
-			{
-				$sub_code = $row['sub_code'];
-				mysqli_query($con, "DELETE from code where sub_code = '$sub_code'");
-				mysqli_query($con, "DELETE from attendance where sub_code = '$sub_code'");
-			}			
-		}		
-		
-		mysqli_query($con, "DELETE from subjects where t_id='$id'");
-		
 		Print '<script>alert("User Deleted!");</script>';
 		Print '<script>window.location.assign("admin.php");</script>';
 	}
