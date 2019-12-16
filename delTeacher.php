43440c3e344492c5f67b0e54b537f332aaa07034
Shivangi Kapadia <31736778+KShivangi@users.noreply.github.com>
2019-12-16 15:38:46 +0530

    Update delTeacher.php

--- delTeacher.php
+++ delTeacher.php
@@ -6,18 +6,6 @@
                 background-color: rgb(233, 233, 255);
             }
             
-            button {
-                display: inline-block;
-				width: 300px;
-                padding: 8px 20px;
-                border: 1px solid black;
-                background-color: rgb(170, 200, 214);
-                color: rgb(0, 0, 0);
-                margin: 8px 30px;
-                border: none;
-                cursor: pointer;
-            }
-
             button:hover {
                 opacity: 0.8;
             }
@@ -28,17 +16,17 @@
         </style>
     </head>
 	
-	<body style="background-color:rgb(229,255,204); color:black">
-		<form action="delStu1.php" method="POST">
-			<div class="container" align="center" style="margin-top: 15%;>
-				<table border="0.5" >
+	<body style="background-image:url('back2.jpg');">
+		<form action="delTeacher1.php" method="POST">
+			<div class="container" style="margin-top: 15%;">
+				<table border="0.5" style="width: 70%;">
 					
 					<tr>
-						<td><label for="id">ID</label></td>
-						<td><input type="text" name="id" id="id" style="width: 300px;"></td>
+						<td><label for="id" style="float: right; margin-right: 5%;">ID</label></td>
+						<td><input type="text" name="id" id="id" style="width: 120%"></td>
 					</tr>
 					<tr>
-						<td><input type="submit" value="Submit" />				
+						<td><input type="submit" value="Submit"  style="margin-top: 10%; margin-left: 400%; padding: 0% 10%;"/>				
 					</tr>
 				</table>
 			</div>
